f9c6296b95496700ea81fcdc7ea4f475b049833f
Динил Иванов <ivanovdaniil551@gmail.com>
2024-03-23 20:10:06 +0300

    Required

--- Programming/Programming/View/MainForm.Designer.cs
+++ Programming/Programming/View/MainForm.Designer.cs
@@ -31,12 +31,22 @@ namespace Programming
         {
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.Enums = new System.Windows.Forms.TabPage();
-            this.listBox1 = new System.Windows.Forms.ListBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.listBox2 = new System.Windows.Forms.ListBox();
-            this.label2 = new System.Windows.Forms.Label();
+            this.IntLabel = new System.Windows.Forms.Label();
+            this.IntTextBox = new System.Windows.Forms.TextBox();
+            this.ValueLabel = new System.Windows.Forms.Label();
+            this.ValueListBox = new System.Windows.Forms.ListBox();
+            this.EnumsLabel = new System.Windows.Forms.Label();
+            this.EnumsListBox = new System.Windows.Forms.ListBox();
+            this.FirstStageGroudBox = new System.Windows.Forms.GroupBox();
+            this.SecondStageGroundBox = new System.Windows.Forms.GroupBox();
+            this.ParseButton = new System.Windows.Forms.Button();
+            this.ParseWeekDayTextBox = new System.Windows.Forms.TextBox();
+            this.ParseLabel = new System.Windows.Forms.Label();
+            this.ParseResultLabel = new System.Windows.Forms.Label();
             this.tabControl1.SuspendLayout();
             this.Enums.SuspendLayout();
+            this.FirstStageGroudBox.SuspendLayout();
+            this.SecondStageGroundBox.SuspendLayout();
             this.SuspendLayout();
             // 
             // tabControl1
@@ -46,68 +56,159 @@ namespace Programming
             this.tabControl1.Location = new System.Drawing.Point(0, 0);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(984, 561);
+            this.tabControl1.Size = new System.Drawing.Size(1234, 711);
             this.tabControl1.TabIndex = 0;
             // 
             // Enums
             // 
-            this.Enums.Controls.Add(this.label2);
-            this.Enums.Controls.Add(this.listBox2);
-            this.Enums.Controls.Add(this.label1);
-            this.Enums.Controls.Add(this.listBox1);
+            this.Enums.Controls.Add(this.SecondStageGroundBox);
+            this.Enums.Controls.Add(this.FirstStageGroudBox);
             this.Enums.Location = new System.Drawing.Point(4, 22);
             this.Enums.Name = "Enums";
             this.Enums.Padding = new System.Windows.Forms.Padding(3);
-            this.Enums.Size = new System.Drawing.Size(976, 535);
+            this.Enums.Size = new System.Drawing.Size(1226, 685);
             this.Enums.TabIndex = 0;
             this.Enums.Text = "Enums";
             this.Enums.UseVisualStyleBackColor = true;
             // 
-            // listBox1
+            // IntLabel
             // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(17, 31);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(130, 199);
-            this.listBox1.TabIndex = 0;
+            this.IntLabel.AutoSize = true;
+            this.IntLabel.Location = new System.Drawing.Point(321, 30);
+            this.IntLabel.Name = "IntLabel";
+            this.IntLabel.Size = new System.Drawing.Size(51, 13);
+            this.IntLabel.TabIndex = 5;
+            this.IntLabel.Text = "Int value:";
             // 
-            // label1
+            // IntTextBox
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(17, 12);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(107, 13);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Choose enumeration:";
+            this.IntTextBox.Location = new System.Drawing.Point(324, 46);
+            this.IntTextBox.Name = "IntTextBox";
+            this.IntTextBox.Size = new System.Drawing.Size(100, 20);
+            this.IntTextBox.TabIndex = 4;
             // 
-            // listBox2
+            // ValueLabel
             // 
-            this.listBox2.FormattingEnabled = true;
-            this.listBox2.Location = new System.Drawing.Point(176, 31);
-            this.listBox2.Name = "listBox2";
-            this.listBox2.Size = new System.Drawing.Size(130, 199);
-            this.listBox2.TabIndex = 2;
+            this.ValueLabel.AutoSize = true;
+            this.ValueLabel.Location = new System.Drawing.Point(168, 30);
+            this.ValueLabel.Name = "ValueLabel";
+            this.ValueLabel.Size = new System.Drawing.Size(75, 13);
+            this.ValueLabel.TabIndex = 3;
+            this.ValueLabel.Text = "Choose value:";
             // 
-            // label2
+            // ValueListBox
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(173, 12);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(75, 13);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "Choose value:";
+            this.ValueListBox.FormattingEnabled = true;
+            this.ValueListBox.Location = new System.Drawing.Point(171, 46);
+            this.ValueListBox.Name = "ValueListBox";
+            this.ValueListBox.ScrollAlwaysVisible = true;
+            this.ValueListBox.Size = new System.Drawing.Size(130, 199);
+            this.ValueListBox.TabIndex = 2;
+            this.ValueListBox.SelectedIndexChanged += new System.EventHandler(this.ValueListBox_SelectedIndexChanged);
+            // 
+            // EnumsLabel
+            // 
+            this.EnumsLabel.AutoSize = true;
+            this.EnumsLabel.Location = new System.Drawing.Point(16, 30);
+            this.EnumsLabel.Name = "EnumsLabel";
+            this.EnumsLabel.Size = new System.Drawing.Size(107, 13);
+            this.EnumsLabel.TabIndex = 1;
+            this.EnumsLabel.Text = "Choose enumeration:";
+            // 
+            // EnumsListBox
+            // 
+            this.EnumsListBox.FormattingEnabled = true;
+            this.EnumsListBox.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.EnumsListBox.Items.AddRange(new object[] {
+            "Color",
+            "Form_edu",
+            "Genre",
+            "Saeson",
+            "Smartphones",
+            "Weekday"});
+            this.EnumsListBox.Location = new System.Drawing.Point(19, 46);
+            this.EnumsListBox.Name = "EnumsListBox";
+            this.EnumsListBox.ScrollAlwaysVisible = true;
+            this.EnumsListBox.Size = new System.Drawing.Size(130, 199);
+            this.EnumsListBox.TabIndex = 0;
+            this.EnumsListBox.SelectedIndexChanged += new System.EventHandler(this.EnumsListBox_SelectedIndexChanged);
+            // 
+            // FirstStageGroudBox
+            // 
+            this.FirstStageGroudBox.Controls.Add(this.IntTextBox);
+            this.FirstStageGroudBox.Controls.Add(this.IntLabel);
+            this.FirstStageGroudBox.Controls.Add(this.EnumsLabel);
+            this.FirstStageGroudBox.Controls.Add(this.EnumsListBox);
+            this.FirstStageGroudBox.Controls.Add(this.ValueListBox);
+            this.FirstStageGroudBox.Controls.Add(this.ValueLabel);
+            this.FirstStageGroudBox.Location = new System.Drawing.Point(8, 20);
+            this.FirstStageGroudBox.Name = "FirstStageGroudBox";
+            this.FirstStageGroudBox.Size = new System.Drawing.Size(504, 306);
+            this.FirstStageGroudBox.TabIndex = 6;
+            this.FirstStageGroudBox.TabStop = false;
+            this.FirstStageGroudBox.Text = "Enumerations";
+            // 
+            // SecondStageGroundBox
+            // 
+            this.SecondStageGroundBox.Controls.Add(this.ParseResultLabel);
+            this.SecondStageGroundBox.Controls.Add(this.ParseLabel);
+            this.SecondStageGroundBox.Controls.Add(this.ParseWeekDayTextBox);
+            this.SecondStageGroundBox.Controls.Add(this.ParseButton);
+            this.SecondStageGroundBox.Location = new System.Drawing.Point(9, 397);
+            this.SecondStageGroundBox.Name = "SecondStageGroundBox";
+            this.SecondStageGroundBox.Size = new System.Drawing.Size(392, 217);
+            this.SecondStageGroundBox.TabIndex = 7;
+            this.SecondStageGroundBox.TabStop = false;
+            this.SecondStageGroundBox.Text = "Weekday Parsing";
+            // 
+            // ParseButton
+            // 
+            this.ParseButton.Location = new System.Drawing.Point(249, 103);
+            this.ParseButton.Name = "ParseButton";
+            this.ParseButton.Size = new System.Drawing.Size(75, 23);
+            this.ParseButton.TabIndex = 0;
+            this.ParseButton.Text = "Parse";
+            this.ParseButton.UseVisualStyleBackColor = true;
+            this.ParseButton.Click += new System.EventHandler(this.ParseButton_Click);
+            // 
+            // ParseWeekDayTextBox
+            // 
+            this.ParseWeekDayTextBox.Location = new System.Drawing.Point(46, 106);
+            this.ParseWeekDayTextBox.Name = "ParseWeekDayTextBox";
+            this.ParseWeekDayTextBox.Size = new System.Drawing.Size(155, 20);
+            this.ParseWeekDayTextBox.TabIndex = 1;
+            // 
+            // ParseLabel
+            // 
+            this.ParseLabel.AutoSize = true;
+            this.ParseLabel.Location = new System.Drawing.Point(46, 87);
+            this.ParseLabel.Name = "ParseLabel";
+            this.ParseLabel.Size = new System.Drawing.Size(115, 13);
+            this.ParseLabel.TabIndex = 2;
+            this.ParseLabel.Text = "Type value for parsing:";
+            // 
+            // ParseResultLabel
+            // 
+            this.ParseResultLabel.AutoSize = true;
+            this.ParseResultLabel.Location = new System.Drawing.Point(49, 133);
+            this.ParseResultLabel.Name = "ParseResultLabel";
+            this.ParseResultLabel.Size = new System.Drawing.Size(0, 13);
+            this.ParseResultLabel.TabIndex = 3;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(984, 561);
+            this.ClientSize = new System.Drawing.Size(1234, 711);
             this.Controls.Add(this.tabControl1);
             this.Name = "MainForm";
             this.Text = "Form1";
             this.tabControl1.ResumeLayout(false);
             this.Enums.ResumeLayout(false);
-            this.Enums.PerformLayout();
+            this.FirstStageGroudBox.ResumeLayout(false);
+            this.FirstStageGroudBox.PerformLayout();
+            this.SecondStageGroundBox.ResumeLayout(false);
+            this.SecondStageGroundBox.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -116,10 +217,18 @@ namespace Programming
 
         private System.Windows.Forms.TabControl tabControl1;
         private System.Windows.Forms.TabPage Enums;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.ListBox listBox1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label EnumsLabel;
+        private System.Windows.Forms.ListBox EnumsListBox;
+        private System.Windows.Forms.Label ValueLabel;
+        private System.Windows.Forms.ListBox ValueListBox;
+        private System.Windows.Forms.Label IntLabel;
+        private System.Windows.Forms.TextBox IntTextBox;
+        private System.Windows.Forms.GroupBox FirstStageGroudBox;
+        private System.Windows.Forms.GroupBox SecondStageGroundBox;
+        private System.Windows.Forms.Label ParseResultLabel;
+        private System.Windows.Forms.Label ParseLabel;
+        private System.Windows.Forms.TextBox ParseWeekDayTextBox;
+        private System.Windows.Forms.Button ParseButton;
     }
 }
 
--- Programming/Programming/View/MainForm.cs
+++ Programming/Programming/View/MainForm.cs
@@ -12,9 +12,82 @@ namespace Programming
 {
     public partial class MainForm : Form
     {
+        bool flag = true;
         public MainForm()
         {
             InitializeComponent();
+            EnumsListBox.SelectedIndex = 0;
+        }
+
+        private void EnumsListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch(EnumsListBox.SelectedIndex)
+            {
+                case 0:
+                    ValueListBox.Items.Clear();
+                    foreach(var i in Enum.GetValues(typeof(Color)))
+                    {
+                        ValueListBox.Items.Add(i);
+                    }
+                    break;
+                case 1:
+                    ValueListBox.Items.Clear();
+                    foreach (var i in Enum.GetValues(typeof(Form_edu)))
+                    {
+                        ValueListBox.Items.Add(i);
+                    }
+                    break;
+                case 2:
+                    ValueListBox.Items.Clear();
+                    foreach (var i in Enum.GetValues(typeof(Genre)))
+                    {
+                        ValueListBox.Items.Add(i);
+                    }
+                    break;
+                case 3:
+                    ValueListBox.Items.Clear();
+                    foreach (var i in Enum.GetValues(typeof(Season)))
+                    {
+                        ValueListBox.Items.Add(i);
+                    }
+                    break;
+                case 4:
+                    ValueListBox.Items.Clear();
+                    foreach (var i in Enum.GetValues(typeof(Smartphones)))
+                    {
+                        ValueListBox.Items.Add(i);
+                    }
+                    break;
+                case 5:
+                    ValueListBox.Items.Clear();
+                    foreach (var i in Enum.GetValues(typeof(Weekday)))
+                    {
+                        ValueListBox.Items.Add(i);
+                    }
+                    break;
+            }
+        }
+
+        private void ValueListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            IntTextBox.Text = Convert.ToString(Convert.ToInt32(ValueListBox.Items[ValueListBox.SelectedIndex]));
+        }
+
+        private void ParseButton_Click(object sender, EventArgs e)
+        {
+            foreach(var i in Enum.GetValues(typeof(Weekday)))
+            {
+                if(Convert.ToString(i) == ParseWeekDayTextBox.Text)
+                {
+                    ParseResultLabel.Text = "Это день недели(" + Convert.ToString(i) + " = " + Convert.ToString(Convert.ToInt32(i)) + ")";
+                    flag = false;
+                }
+            }
+            if(flag==true)
+            {
+                ParseResultLabel.Text = "Нет такого дня недели";
+            }
+            flag = true;
         }
     }
 }
