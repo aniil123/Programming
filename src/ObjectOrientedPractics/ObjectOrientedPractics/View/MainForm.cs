916fedbf53f8d97fa61b44dec2f962f30970cd4f
Динил Иванов <ivanovdaniil551@gmail.com>
2024-09-25 21:51:42 +0300

    Required

--- src/ObjectOrientedPractics/ObjectOrientedPractics/View/MainForm.cs
+++ src/ObjectOrientedPractics/ObjectOrientedPractics/View/MainForm.cs
@@ -13,7 +13,7 @@ namespace ObjectOrientedPractics
     public partial class MainForm : Form
     {
         public MainForm()
-        {
+        {//
             InitializeComponent();
         }
     }
