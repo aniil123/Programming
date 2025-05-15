812de6e2662939fb2e07191870e111c2bb2f8d5a
Динил Иванов <ivanovdaniil551@gmail.com>
2025-05-15 14:22:51 +0700

    Required

--- src/Contacts/View/ViewModel/Commands/AddCommand.cs
+++ src/Contacts/View/ViewModel/Commands/AddCommand.cs
@@ -14,37 +14,9 @@ namespace View.ViewModel
     public class AddCommand : DependencyObject, ICommand
     {
         /// <summary>
-        /// Свойство зависимости, хранящее значение, определяющее, может ли быть использована эта команда.
-        /// </summary>
-        public static readonly DependencyProperty EnabledProperty;
-
-        /// <summary>
         /// Событие, которое вызывается при изменении возращаемого значения метода CanExecute.
         /// </summary>
         public event EventHandler CanExecuteChanged;
-
-        /// <summary>
-        /// Возвращает и задает значение свойства зависимости EnabledProperty.
-        /// </summary>
-        public bool Enabled
-        {
-            get { return (bool)GetValue(EnabledProperty); }
-            set { SetValue(EnabledProperty, value); }
-        }
-
-        /// <summary>
-        /// Обработчик события, которое вызывается при изменении значений объектов <see cref="DependencyProperty"/> этого класса.
-        /// </summary>
-        /// <param name="dependencyObject">Экземпляр этого класса, свойство которого изменилось.</param>
-        /// <param name="e"></param>
-        private static void Enabled_Changed(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs e)
-        {
-            AddCommand addCommand = (AddCommand)dependencyObject;
-            if(addCommand.CanExecuteChanged != null)
-            {
-                addCommand.CanExecuteChanged(addCommand, new EventArgs());
-            }
-        }
 
         public void Execute(object parameter)
         {
@@ -58,15 +30,7 @@ namespace View.ViewModel
 
         public bool CanExecute(object parameter)
         {
-            return Enabled;
-        }
-
-        /// <summary>
-        /// Регистрирует свойство зависимости EnabledProperty.
-        /// </summary>
-        static AddCommand()
-        {
-            EnabledProperty = DependencyProperty.Register("Enabled", typeof(bool), typeof(AddCommand), new PropertyMetadata(Enabled_Changed));
+            return true;
         }
     }
 }
