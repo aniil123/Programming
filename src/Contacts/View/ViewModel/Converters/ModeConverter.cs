082a3c9191d6b745aa805de242c967e60c85e2f9
Динил Иванов <ivanovdaniil551@gmail.com>
2025-05-15 15:11:04 +0700

    Required

---
+++ src/Contacts/View/ViewModel/Converters/ModeConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+using System.Globalization;
+
+namespace View.ViewModel.Converters
+{
+    class ModeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(targetType == typeof(bool))
+                return (Modes)value == Modes.Viewing;
+            return (Modes)value == Modes.Viewing ? Visibility.Hidden : Visibility.Visible;
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
